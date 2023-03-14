02f122f42a5c3e0da7fb5b7266d92ad330d0670a
haritiana adriano <hei.adriano.4@gmail.com>
2023-03-14 15:17:39 +0300

    feat: film document to store in mongodb

--- src/main/java/com/data/research/model/FilmDocument.java
+++ src/main/java/com/data/research/model/FilmDocument.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.bson.types.Binary;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
@@ -15,6 +16,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "Film")
 public class FilmDocument {
     @Id
-    private int _id;
+    private String _id;
     private String title;
+    private Binary image;
 }
