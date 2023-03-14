4d86bf91294470d1f790b097047bdba101f51a24
haritiana adriano <hei.adriano.4@gmail.com>
2023-03-14 15:57:06 +0300

    feat: postMapping for image service

--- src/main/java/com/data/research/resource/FilmDocumentController.java
+++ src/main/java/com/data/research/resource/FilmDocumentController.java
@@ -4,10 +4,10 @@ import com.data.research.model.FilmDocument;
 import com.data.research.service.FilmDocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Base64;
 
 @RestController
@@ -25,5 +25,15 @@ public class FilmDocumentController {
         model.addAttribute("image",
                 Base64.getEncoder().encodeToString(filmDocument.getImage().getData()));
         return "photo";
+    }
+
+    @PostMapping("/photo")
+    public String addFilmDocument(
+            @RequestParam(name = "title")String title,
+            @RequestParam(name = "image")MultipartFile newFilmDocument,
+            Model model
+    ) throws IOException {
+        String id = service.insertFilmDocument(title, newFilmDocument);
+        return("redirect: /photo/"+id);
     }
 }
