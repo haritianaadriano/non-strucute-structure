94cb0a5d370afa7d54c02a9b723786672e028e8e
haritiana adriano <hei.adriano.4@gmail.com>
2023-03-14 15:39:23 +0300

    feat: service of file adding and getting

---
+++ src/main/java/com/data/research/controller/FilmDocumentService.java
@@ -0,0 +1,35 @@
+package com.data.research.controller;
+
+import com.data.research.model.FilmDocument;
+import com.data.research.repository.FilmDocumentRepository;
+import org.bson.BsonBinarySubType;
+import org.bson.types.Binary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+public class FilmDocumentService {
+    @Autowired
+    private FilmDocumentRepository repository;
+
+    //SAVE image
+    public String insertFilmDocument(String title, MultipartFile file) throws IOException {
+        FilmDocument newFilmDocument = new FilmDocument();
+        newFilmDocument.setTitle(title);
+        newFilmDocument.setImage(
+                new Binary(
+                BsonBinarySubType.BINARY,
+                file.getBytes())
+        );
+        newFilmDocument = repository.insert(newFilmDocument);
+        return("the file with id#"+newFilmDocument.get_id()+" was store successfully");
+    }
+
+    //GET image
+    public FilmDocument getFilmDocument(String id){
+        return repository.findById(id).get();
+    }
+}
--- src/main/java/com/data/research/model/FilmDocument.java
+++ src/main/java/com/data/research/model/FilmDocument.java
@@ -1,17 +1,13 @@
 package com.data.research.model;
 
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.bson.types.Binary;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 
 @Document(collection = "Film")
 public class FilmDocument {
