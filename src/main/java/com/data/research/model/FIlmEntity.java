6a0d87c775315b1c1cd55e8e3edd5a232cf2f6d4
haritiana adriano <hei.adriano.4@gmail.com>
2023-03-28 08:00:29 +0300

    feat: filmEntity on model

--- src/main/java/com/data/research/model/FIlmEntity.java
+++ src/main/java/com/data/research/model/FIlmEntity.java
@@ -1,15 +1,25 @@
 package com.data.research.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name = "film")
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "film")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class FIlmEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private int id;
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = true)
+    private AuthorEntity author;
 }
