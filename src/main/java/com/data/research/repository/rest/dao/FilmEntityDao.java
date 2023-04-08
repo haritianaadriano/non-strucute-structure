a68fc1cfe473ec4314eccce6a17dda9ebbdb5e97
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-08 17:28:36 +0300

    chore: instantiating the dao function

--- src/main/java/com/data/research/repository/rest/dao/FilmEntityDao.java
+++ src/main/java/com/data/research/repository/rest/dao/FilmEntityDao.java
@@ -1,4 +1,23 @@
 package com.data.research.repository.rest.dao;
 
-public class FilmEntityDao extends {
+import com.data.research.model.rest.FIlmEntity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@AllArgsConstructor
+public class FilmEntityDao{
+    private EntityManager entityManager;
+
+    public List<FIlmEntity> findByCriteria(String authorName, int idAuthor){
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<FIlmEntity> query = builder.createQuery(FIlmEntity.class);
+        Root<FIlmEntity> root = query.from(FIlmEntity.class);
+    }
 }
