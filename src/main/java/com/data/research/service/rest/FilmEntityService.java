ddde085b08da14669aeb940055e261d528cfea87
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-08 17:48:33 +0300

    feat: getFilmBuAuthorName in service

--- src/main/java/com/data/research/service/rest/FilmEntityService.java
+++ src/main/java/com/data/research/service/rest/FilmEntityService.java
@@ -2,6 +2,7 @@ package com.data.research.service.rest;
 
 import com.data.research.model.rest.FIlmEntity;
 import com.data.research.repository.rest.FilmEntityRepository;
+import com.data.research.repository.rest.dao.FilmEntityDao;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 public class FilmEntityService {
     private FilmEntityRepository repository;
+    private FilmEntityDao dao;
 
     public List<FIlmEntity> saveAll(List<FIlmEntity> toSave){
         return repository.saveAll(toSave);
@@ -19,5 +21,9 @@ public class FilmEntityService {
 
     public List<FIlmEntity> getAll(){
         return repository.findAll();
+    }
+
+    public List<FIlmEntity> getFilmeByAuthorName(String name){
+        return dao.findByCriteria(name);
     }
 }
