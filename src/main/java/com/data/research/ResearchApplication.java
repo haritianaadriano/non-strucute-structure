ecfd50039ca675a205f910ea8cbd5e660204e8cd
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-21 11:26:30 +0300

    refactor: auto configuration of security

--- src/main/java/com/data/research/ResearchApplication.java
+++ src/main/java/com/data/research/ResearchApplication.java
@@ -2,8 +2,9 @@ package com.data.research;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ResearchApplication {
 
 	public static void main(String[] args) {
