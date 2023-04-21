97da4668db16ded8286f1c4678817cb373185e9a
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-21 11:11:22 +0300

    feat: data

---
+++ src/main/resources/db/migration/V0_3__Insert_data.sql
@@ -0,0 +1,7 @@
+--insertion for author table
+insert into "author" (name) values
+('author1'), ('author2');
+
+--insertion for film table
+insert into film (name, author_id) values
+('film1', 1), ('film2', 1), ('film3', 2);
