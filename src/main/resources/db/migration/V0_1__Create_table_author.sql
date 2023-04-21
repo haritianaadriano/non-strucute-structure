43b7d42b12b7a74a23e35b243fa7247f1a4e7fc4
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-21 11:16:15 +0300

    fixup: serial type

--- src/main/resources/db/migration/V0_1__Create_table_author.sql
+++ src/main/resources/db/migration/V0_1__Create_table_author.sql
@@ -1,4 +1,4 @@
 create table if not exists "author" (
-    id int serial primary key not null,
+    id serial primary key not null,
     name varchar not null
 )
--- src/main/resources/db/migration/V0_2__Create_table_film.sql
+++ src/main/resources/db/migration/V0_2__Create_table_film.sql
@@ -1,5 +1,5 @@
 create table if not exists film (
-    id int serial primary key,
+    id serial primary key,
     name varchar not null,
     author_id int references author(id)
 )
