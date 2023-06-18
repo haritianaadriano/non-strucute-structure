ac401013d665ae91d6e2b468b729a5d573c43ff8
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-06-18 21:54:27 +0300

    feat: basic configuration for security

---
+++ src/main/java/com/data/research/conf/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.data.research.conf;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
